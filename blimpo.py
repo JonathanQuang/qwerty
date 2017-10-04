f0d9fd9a93c014a730c4473fd6fc0ab84389138e
JonathanQuang <jquang@stuy.edu>
2017-10-04 13:57:11 -0400

    if else statements

--- blimpo.py
+++ blimpo.py
@@ -1,12 +1,19 @@
-import random
-from flask import Flask, render_template
+import random, hashlib
+from flask import Flask, render_template, request
+
+defaultUser = "Bob"
+defaultPassword = "123" 
 
 
 app=Flask(__name__)
 
 @app.route('/')
 def occu():
-	return render_template("login.html")
+	if len(request.args) == 0:
+		return render_template("login.html")
+	else:
+		if (request.args["username"]=="Bob") and (request.args["password"]=="123"):
+			return render_template("loggedIn.html")
 
 if __name__ == '__main__':
 	app.debug==True
