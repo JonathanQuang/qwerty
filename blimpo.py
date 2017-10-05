f1947929d25470a55d0b65288204813c9f3a9ac1
clivejj <cjohnston1@stuy.edu>
2017-10-04 22:37:10 -0400

    finished

--- blimpo.py
+++ blimpo.py
@@ -1,7 +1,7 @@
 import random, hashlib, os
 from flask import Flask, render_template, request, session
 
-#set default user/password for testing, cause assignment never asks for a register form
+#set default user/password for testing
 defaultUser = "Bob"
 defaultPassword = "123" 
 
@@ -10,18 +10,30 @@ app=Flask(__name__)
 app.secret_key=os.urandom(32)
 
 @app.route('/',methods=["GET","POST"])
-def occu():
-	#render login page if the cookie is empty and there is no form data
-	#i done goofed
-	if (len(request.form) == 0) and (len(session)==0):
-		return render_template("login.html")
+def root():
+	#render loggedIn page if session has "user":"Bob"
+	if len(session)==1 and  session["user"]=="Bob":
+		return render_template("loggedIn.html")
+        #otherwise, render login page
 	else:
-		if (request.form["username"]=="Bob") and (request.form["password"]=="123") and len(session)==0:
-			session["user"]="Bob"
-			session["password"]="123"
-		if (session["user"]=="Bob") and (session["password"]=="123"):
-			print "cookie works"
-			return render_template("loggedIn.html")
+                return render_template("login.html")
+
+@app.route("/entry", methods=["GET", "POST"])	      
+def entry():
+        #if correct info is entered thru form, store session, and render loggedIn page
+        if (request.form["username"]=="Bob") and (request.form["password"]=="123"):
+                session["user"] = "Bob"
+                return render_template("loggedIn.html")
+        #otherwise if username/password combo is wrong, render error page
+        else:
+                return render_template("error.html")
+
+@app.route("/logOff", methods=["GET", "POST"])
+def logOff():
+        #if logoff button is hit, remove session, and render logoff page
+        session.pop("user")
+        return render_template("loggedOff.html")
+        
 
 if __name__ == '__main__':
 	app.debug==True
